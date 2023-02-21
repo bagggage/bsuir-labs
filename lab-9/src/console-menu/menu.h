c33234178ccf2b2cf20a2e7e14e57d891432a674
Kostya Pigulevskiy <pigulevskiykostya@gmail.com>
2023-02-21 22:43:46 +0300

    Added simple menu API, and implemented handler for console

---
+++ lab-9/src/console-menu/menu.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "../parser/bool.h"
+
+typedef void(*FunctionPtr)(void);
+typedef void** Args;
+
+typedef struct FunctionBind
+{
+	FunctionPtr functionPtr;
+	Args args;
+
+	unsigned int argsCount;
+	
+} FunctionBind;
+
+typedef struct MenuItem 
+{
+	const char* name;
+	FunctionBind function;
+
+} MenuItem;
+
+/*Main structure for implementing menu in your application.
+Don't create instances directly, use 'init' function.
+Clear data using 'destroy' function after work is done.
+Also use special functions for setting up and handlind event*/
+typedef struct Menu 
+{
+	MenuItem* menuItems;
+	size_t count;
+
+	BOOL isSubmenu;
+
+} Menu;
+
+Menu initMenu();
+FunctionBind funcBind(FunctionPtr function, unsigned int argsCount, ...);
+
+void destroyMenu(Menu* menu);
+void addMenuItem(Menu* menu, const char* name, FunctionBind function);
+void addSubmenu(Menu* menu, const Menu* submenu);
+void executeFunction(const FunctionBind* function);
